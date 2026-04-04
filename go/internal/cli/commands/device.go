4052a32f87764dbf8b676b906c098a79f19682b2
Joannis Orlandos <joannis@orlandos.nl>
2026-04-04 19:03:08 +0200

    Move `wendy audio` under `wendy device ...`

--- go/internal/cli/commands/device.go
+++ go/internal/cli/commands/device.go
@@ -53,6 +53,7 @@ func newDeviceCmd() *cobra.Command {
 		newWifiCmd(),
 		newAppsCmd(),
 		newVolumesCmd(),
+		newAudioCmd(),
 	)
 
 	return cmd
--- go/internal/cli/commands/root.go
+++ go/internal/cli/commands/root.go
@@ -102,8 +102,6 @@ func NewRootCmd() *cobra.Command {
 	discoverCmd.GroupID = "devices"
 	osCmd := newOSCmd()
 	osCmd.GroupID = "devices"
-	audioCmd := newAudioCmd()
-	audioCmd.GroupID = "devices"
 	bluetoothCmd := newBluetoothCmd()
 	bluetoothCmd.GroupID = "devices"
 	hardwareCmd := newHardwareCmd()
@@ -141,7 +139,6 @@ func NewRootCmd() *cobra.Command {
 		deviceCmd,
 		discoverCmd,
 		osCmd,
-		audioCmd,
 		bluetoothCmd,
 		hardwareCmd,
 		cacheCmd,
